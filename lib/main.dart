b2d1f4bd68214d40051001914082e007ed5bf438
emalianakasmuri <emaliana.kasmuri@gmail.com>
2024-09-26 01:16:04 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+/**
+ * Purpose:
+ * This app demontsrate the used of Row to display a set of data.
+ */
+
+/**
+ * Main entry point to the app
+ */
+void main() {
+
+  // Instantiate and run the application
+  runApp( RowApp01());
+}
+
+// This class represents the app
+class RowApp01 extends StatelessWidget {
+
+  /**
+   * This method builds the UI
+   */
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+
+      // Scaffold definition to lay the UI widgets
+      home: Scaffold(
+
+        // AppBar definition
+        appBar: AppBar(
+          title: Text("Row App 01"),
+        ),
+
+        // Row definition
+        body: Row(
+
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+          // Display content of row
+          children: <Widget>[
+            Icon(Icons.radio_button_checked),
+            Text("Upload lecture slides on ulearn"),
+            Icon(Icons.flag)
+          ],
+        ),
+
+      ),
+
+      // Turn off the debug banner
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
+
+
+
+
+
+
