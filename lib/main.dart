ed57c4c888e20aabd5025dde488510db3ab7fce9
emalianakasmuri <emaliana.kasmuri@gmail.com>
2024-10-02 09:25:34 +0800

    Update main.dart
    Update the comment style

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 
-/**
- * Purpose:
- * This app demontsrate the used of Row to display a set of data.
- */
 
-/**
- * Main entry point to the app
- */
+/// BITP 3453 Mobile Application Development
+/// Lecture 02
+/// Author: Emaliana Kasmuri
+///
+/// Purpose:
+/// This app demonstrate the used of Row to display a set of data.
+
+
+/// Main entry point to the app
 void main() {
 
   // Instantiate and run the application
   runApp( RowApp01());
 }
 
-// This class represents the app
+/// This class represents the app
 class RowApp01 extends StatelessWidget {
 
-  /**
-   * This method builds the UI
-   */
+  /// This method builds the UI
   @override
   Widget build(BuildContext context) {
 
@@ -50,6 +50,7 @@ class RowApp01 extends StatelessWidget {
 
       // Turn off the debug banner
       debugShowCheckedModeBanner: false,
+
     );
   }
 }
